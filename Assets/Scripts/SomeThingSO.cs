13a991153e9da18eedcd6e495513c4c49da23f30
jabriezhang <15688641+jukrb0x@users.noreply.github.com>
2022-07-14 20:17:09 +0800

    feat(plugin): add SO browser

---
+++ Assets/Scripts/SomeThingSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName = "Create SomeThingSO", fileName = "SomeThingSO", order = 0)]
+    public class SomeThingSO : ScriptableObject
+    {
+        
+    }
+}
