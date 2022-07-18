a9d57c06487072b678638d88c9c174cf4f749f35
jabriezhang <15688641+jukrb0x@users.noreply.github.com>
2022-07-18 19:11:34 +0800

    feat: build level scene

--- Assets/Scripts/Game/Gameplay/BaseTrigger.cs
+++ Assets/Scripts/Game/Gameplay/BaseTrigger.cs
@@ -1,9 +1,33 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Game.Gameplay
 {
     public class BaseTrigger : MonoBehaviour
     {
-        
+        // fixme: test code
+        private TextMeshProUGUI text;
+        [SerializeField] private GameObject player;
+        private Collider _collider;
+
+
+        private void Start()
+        {
+            // player = GameObject.FindWithTag("Player");
+            _collider = player.GetComponent<Collider>();
+            text = GameObject.Find("DebugMsg").GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            OnTriggerStay(_collider);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            text.SetText("You have entered the trigger");
+        }
     }
 }
