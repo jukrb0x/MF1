2e06e272dae87c7e0fb9c75ca3043430a2d29569
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-08-04 18:19:17 +0800

    fix: math and utilities

--- Assets/Scripts/Gameplay/Ragdoll/Core/Body.cs
+++ Assets/Scripts/Gameplay/Ragdoll/Core/Body.cs
@@ -233,4 +233,32 @@ namespace Gameplay.Ragdoll.Core
             };
         }
     }
+    
+    [Serializable]
+    public struct JointMotionsConfig // todo: will need when grab things
+    {
+        public ConfigurableJointMotion angularXMotion, angularYMotion, angularZMotion;
+        public float                   angularXLimit,  angularYLimit,  angularZLimit;
+
+        public void ApplyTo(ref ConfigurableJoint joint)
+        {
+            joint.angularXMotion = angularXMotion;
+            joint.angularYMotion = angularYMotion;
+            joint.angularZMotion = angularZMotion;
+
+            var softJointLimit = new SoftJointLimit();
+
+            softJointLimit.limit = angularXLimit / 2;
+            joint.highAngularXLimit = softJointLimit;
+
+            softJointLimit.limit = -softJointLimit.limit;
+            joint.lowAngularXLimit = softJointLimit;
+
+            softJointLimit.limit = angularYLimit;
+            joint.angularYLimit = softJointLimit;
+
+            softJointLimit.limit = angularZLimit;
+            joint.angularZLimit = softJointLimit;
+        }
+    }
 }
--- Assets/Scripts/Gameplay/Ragdoll/Core/RagdollAnimation.cs
+++ Assets/Scripts/Gameplay/Ragdoll/Core/RagdollAnimation.cs
@@ -1,7 +1,25 @@
+using Gameplay.Ragdoll.Utilities;
+using UnityEngine;
+
 namespace Gameplay.Ragdoll.Core
 {
-    public class RagdollAnimation : RagCoreBase
+    public class RagdollAnimation : RagdollCore
     {
+
+        // Inverse Kinematics
+        public bool enableIK = true;
+
+        // Body
+        private Quaternion[]        _initialJointsRotation;
+        private ConfigurableJoint[] _joints;
+        private Transform[]         _animatedBones;
+        private AnimatorHelper      _animatorHelper;
+
+        public Animator animator;
         
+        // Define the rotation range in which the target direction influences arm movement.
+        public float minTargetDirAngle = - 30,
+                     maxTargetDirAngle = 60;
+
     }
 }
