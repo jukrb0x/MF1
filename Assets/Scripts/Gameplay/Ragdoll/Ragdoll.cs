51a7762d5f1f42ca1e5cfbf2eda1ddbbfa5c16e4
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-08-05 10:36:58 +0800

    feat: ragdoll grab

--- Assets/Scripts/Gameplay/Ragdoll/Ragdoll.cs
+++ Assets/Scripts/Gameplay/Ragdoll/Ragdoll.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 
 namespace Gameplay.Ragdoll
 {
-    // The Ragdoll System Entry
-    // Configure body parts and their connections
+    [RequireComponent(typeof(RagdollGrab))]
     [RequireComponent(typeof(RagdollCamera))]
     [RequireComponent(typeof(RagdollMovement))]
     [RequireComponent(typeof(RagdollPhysics))]
     [RequireComponent(typeof(RagdollBody))]
     [RequireComponent(typeof(RagdollAnimation))]
     [RequireComponent(typeof(RagdollInputs))]
+    // The modularized Ragdoll System
     public class Ragdoll : MonoBehaviour
     {
-
         // Ragdoll Components
         [Header("Ragdoll System")]
         public RagdollInputs inputs;
@@ -24,6 +23,7 @@ namespace Gameplay.Ragdoll
         public RagdollPhysics   ragdollPhysics;
         public RagdollMovement  ragdollMovement;
         public RagdollCamera    ragdollCamera;
+        public RagdollGrab      ragdollGrab;
 
         // Ragdoll Internals
 
@@ -35,6 +35,7 @@ namespace Gameplay.Ragdoll
             if (ragdollPhysics == null) ragdollPhysics = GetComponent<RagdollPhysics>();
             if (ragdollMovement == null) ragdollMovement = GetComponent<RagdollMovement>();
             if (ragdollCamera == null) ragdollCamera = GetComponent<RagdollCamera>();
+            if (ragdollGrab == null) ragdollGrab = GetComponent<RagdollGrab>();
         }
 
 
